0eb3cd80963d1eb25b107389bd38150a2d695655
admontero <adavidmontero@outlook.com>
2021-12-18 15:17:51 -0500

    Correcciones

--- src/components/EditProject.js
+++ src/components/EditProject.js
@@ -152,7 +152,7 @@ const EditProject = () => {
                                 <div className="card-footer d-grid d-md-flex justify-content-md-end">
                                     <button type="submit" className="btn btn-dark">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-square-plus" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><rect x="4" y="4" width="16" height="16" rx="2" /><line x1="9" y1="12" x2="15" y2="12" /><line x1="12" y1="9" x2="12" y2="15" /></svg>
-                                        Crear Proyecto
+                                        Actualizar información
                                     </button>
                                 </div>
                             </div>
